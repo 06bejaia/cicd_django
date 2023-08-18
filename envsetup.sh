d58346da43c470ee357dd69fa008e5740e57a132
nacer06 <insses06@gmail.com>
2023-08-18 13:09:46 -0700

    cicid

---
+++ envsetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [-d "env"]
+then
+    echo " folder exsists"
+else
+    python -m venv env
+fi
+
+echo $pwd
+
+pip install -r requirements.txt
+
+
+if [-d "logs"]
+then 
+    echo "folder exists"
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
+echo "setup finished"
