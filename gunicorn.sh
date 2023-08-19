cfac1701e99de520082c8a742fc05ed977785630
nacer06 <insses06@gmail.com>
2023-08-19 06:25:40 -0700

    dd

--- gunicorn.sh
+++ gunicorn.sh
@@ -3,7 +3,7 @@
 source venv/bin/activate
 
 cd /var/lib/jenkins/workspace/Django_cicd/app
-
+pip3 install django
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic -- no-input
