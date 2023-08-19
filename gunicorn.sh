a934e5aa5f09b216108aa17c9b4db86829d4844e
nacer06 <insses06@gmail.com>
2023-08-19 05:59:32 -0700

    dd

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,20 @@
-# !/bin/bash
+#!/bin/bash
 
-env/Scripts/activate
+source env/bin/activate
+
+cd /var/lib/jenkins/workspace/Django_cicd/app
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic -- no-input
+
+echo "Migrations done"
+
+cd /var/lib/jenkins/workspace/Django_cicd
+
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
